92cd55f071aa4b3397ae267af4d37c2345dbb3dc
Dharmesh <dharmesh.srivastava@gmail.com>
2020-11-01 20:09:17 +0530

    Added New Column

--- MyProject.Entities/Models/Employee.cs
+++ MyProject.Entities/Models/Employee.cs
@@ -9,5 +9,7 @@ namespace MyProject.Entities.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+
+        public string Surname { get; set; }
     }
 }
