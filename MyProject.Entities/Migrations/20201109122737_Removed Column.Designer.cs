36d97d7fdc8112a0faf6c37cd46fdc53aff868cd
Dharmesh <dharmesh.srivastava@gmail.com>
2020-11-09 17:58:10 +0530

    Column Removed

--- MyProject.Entities/Migrations/20201109122737_Removed Column.Designer.cs
+++ MyProject.Entities/Migrations/20201109122737_Removed Column.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyProject.Entities;
 
 namespace MyProject.Entities.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20201109122737_Removed Column")]
+    partial class RemovedColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MyProject.Entities/Migrations/20201109122737_Removed Column.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyProject.Entities.Migrations
+{
+    public partial class RemovedColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Surname",
+                table: "Employees");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Surname",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
